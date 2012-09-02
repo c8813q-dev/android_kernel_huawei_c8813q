c339cf25ab527df2093b1495c19621115ad0f107
Steve Kondik <shade@chemlab.org>
2012-09-02 05:12:11 -0700

    cpufreq: ondemand: Fix the boostpulse interface
      * Boostpulse was not working because no default frequency was set.
      * We don't need an input handler to push the freq to max, userspace
        does a better job with the Power HAL.
      * Tweak boost time to 500ms
    
    Change-Id: Ice8c26b758747d265976263296a2da3380b864f7
    
    Conflicts:
    
    	drivers/cpufreq/cpufreq_ondemand.c

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -46,7 +46,7 @@
 #ifdef CONFIG_HUAWEI_KERNEL
 #define MICRO_FREQUENCY_PREFERED_SAMPLE_RATE (30000)
 #endif
-#define DEFAULT_FREQ_BOOST_TIME			(2500000)
+#define DEFAULT_FREQ_BOOST_TIME			(500000)
 
 u64 freq_boosted_time;
 
@@ -156,6 +156,7 @@ static struct dbs_tuners {
 	.ignore_nice = 0,
 	.powersave_bias = 0,
 	.freq_boost_time = DEFAULT_FREQ_BOOST_TIME,
+	.boostfreq = 1512000,
 };
 
 static inline u64 get_cpu_idle_time_jiffy(unsigned int cpu, u64 *wall)
@@ -1046,6 +1047,7 @@ bail_acq_sema_failed:
 	return;
 }
 
+#if 0
 static void dbs_input_event(struct input_handle *handle, unsigned int type,
 		unsigned int code, int value)
 {
@@ -1124,6 +1126,8 @@ static void dbs_input_disconnect(struct input_handle *handle)
 	input_unregister_handle(handle);
 	kfree(handle);
 }
+#endif
+
 static const struct input_device_id dbs_ids[] = {
 /*{ .driver_info = 1 },*/
 	{
@@ -1143,6 +1147,8 @@ static const struct input_device_id dbs_ids[] = {
 	}, /* touchpad */
 
 };
+
+#if 0
 static struct input_handler dbs_input_handler = {
 	.event		= dbs_input_event,
 	.connect	= dbs_input_connect,
@@ -1150,6 +1156,7 @@ static struct input_handler dbs_input_handler = {
 	.name		= "cpufreq_ond",
 	.id_table	= dbs_ids,
 };
+#endif
 
 static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 				   unsigned int event)
@@ -1213,8 +1220,10 @@ static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 #endif
 			dbs_tuners_ins.io_is_busy = should_io_be_busy();
 		}
+#if 0
 		if (!cpu)
 			rc = input_register_handler(&dbs_input_handler);
+#endif
 		mutex_unlock(&dbs_mutex);
 
 		if (!ondemand_powersave_bias_setspeed(
@@ -1233,8 +1242,10 @@ static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 		/* If device is being removed, policy is no longer
 		 * valid. */
 		this_dbs_info->cur_policy = NULL;
+#if 0
 		if (!cpu)
 			input_unregister_handler(&dbs_input_handler);
+#endif
 		mutex_unlock(&dbs_mutex);
 		if (!dbs_enable)
 			sysfs_remove_group(cpufreq_global_kobject,
