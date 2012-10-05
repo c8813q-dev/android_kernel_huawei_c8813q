eade9efefc41fca9dcadf4655de414f257e27489
Kaushal Kumar <kaushalk@codeaurora.org>
2012-10-05 07:25:20 +0530

    msm: cpr: Clear the interrupts on any state change
    Clear the interrupts before enabling CPR sampling and before
    saving state during suspend
    
    Signed-off-by: Kaushal Kumar <kaushalk@codeaurora.org>
    (cherry picked from commit 0aae7433ae51d054bda422f394af92a753eb41ba)
    
    Signed-off-by: Gopal Goberu <ggober@codeaurora.org>
    (cherry picked from commit 15eb77c14814bca14dc6ab2307f4b60888390d66)
    
    Change-Id: I6d9b4a00619c571f0fe91c56c38fa221feecdd59
    Signed-off-by: Sridhar Gujje <sgujje@codeaurora.org>

--- arch/arm/mach-msm/msm_cpr.c
+++ arch/arm/mach-msm/msm_cpr.c
@@ -778,6 +778,9 @@ cpr_freq_transition(struct notifier_block *nb, unsigned long val,
 			"RBIF_IRQ_STATUS: 0x%x\n",
 			cpr_read_reg(cpr, RBIF_IRQ_STATUS));
 
+		/* Clear all the interrupts */
+		cpr_write_reg(cpr, RBIF_IRQ_CLEAR, ALL_CPR_IRQ);
+
 		cpr_enable(cpr);
 		break;
 	default:
@@ -811,6 +814,9 @@ static int msm_cpr_resume(void)
 	cpr_write_reg(cpr, RBCPR_CTL,
 		cpr_save_state.rbcpr_ctl);
 
+	/* Clear all the interrupts */
+	cpr_write_reg(cpr, RBIF_IRQ_CLEAR, ALL_CPR_IRQ);
+
 	enable_irq(cpr->irq);
 	cpr_enable(cpr);
 
@@ -825,6 +831,9 @@ static int msm_cpr_suspend(void)
 	/* Disable CPR measurement before IRQ to avoid pending interrupts */
 	cpr_disable(cpr);
 	disable_irq(cpr->irq);
+
+	/* Clear all the interrupts */
+	cpr_write_reg(cpr, RBIF_IRQ_CLEAR, ALL_CPR_IRQ);
 
 	cpr_save_state.rbif_timer_interval =
 		cpr_read_reg(cpr, RBCPR_TIMER_INTERVAL);
