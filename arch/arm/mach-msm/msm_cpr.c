46771021451d7ff2201ac7d43e57029850c3e693
Tirupathi Reddy <tirupath@codeaurora.org>
2012-12-05 20:55:54 +0530

    msm: cpr: Make disable_cpr true by default
    Rightnow, disable_cpr is initialised to false. But cpr_pm_suspend
    and cpr_pm_resume can be called from outside even if cpr is disabled.
    To be on safe side, initialise disable_cpr to true and make it false
    in probe.
    
    Change-Id: I6366f96948b74cb399b69e1b0f3c8b47d603baf5
    Signed-off-by: Tirupathi Reddy <tirupath@codeaurora.org>

--- arch/arm/mach-msm/msm_cpr.c
+++ arch/arm/mach-msm/msm_cpr.c
@@ -54,7 +54,7 @@ void __iomem *virt_start_ptr;
 static struct platform_device *cpr_pdev;
 
 static bool enable = 1;
-static bool disable_cpr;
+static bool disable_cpr = true;
 module_param(enable, bool, 0644);
 MODULE_PARM_DESC(enable, "CPR Enable");
 
@@ -935,7 +935,6 @@ static int __devinit msm_cpr_probe(struct platform_device *pdev)
 
 	if (pdata->disable_cpr == true) {
 		pr_err("CPR disabled by modem\n");
-		disable_cpr = true;
 		return -EPERM;
 	}
 
@@ -1068,6 +1067,7 @@ static int __devinit msm_cpr_probe(struct platform_device *pdev)
 	cpufreq_register_notifier(&cpr->freq_transition,
 					CPUFREQ_TRANSITION_NOTIFIER);
 
+	disable_cpr = false;
 	pr_info("CPR: driver registered successfully\n");
 
 	return res;
